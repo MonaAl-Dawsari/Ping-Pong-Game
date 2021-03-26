18872f231d496354025e834dae3c17a40ac7f98d
MonaAldaw <monaaldosari16@gmail.com>
2021-03-26 17:02:53 +0300

    Create canvas and drew one paddle

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//select the canvas element and set it inside canvas constent
+const canvas =document.getElementById("pingpong");
+
+//getContext gives us method and properities to draw the canvas
+const context = canvas.getContext("2d");
+
+//fill the paddle
+context.fillStyle ="black";
+
+//size the paddle or postion  
+context.fillRect(100,200,50,75);
